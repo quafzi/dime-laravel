0772c5a5a3d94608ea114b32635fae6c275ec8fb
Thomas Birke <quafzi@netextreme.de>
2014-11-05 16:17:13 +0100

    Call seeds of Dime Core package

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 class DatabaseSeeder extends Seeder {
 
+    use DimeCoreSeederTrait;
+
     /**
      * Run the database seeds.
      *
@@ -11,18 +13,6 @@ class DatabaseSeeder extends Seeder {
     {
         Eloquent::unguard();
 
-        DB::table('timeslices')->delete();
-        DB::table('activities')->delete();
-        DB::table('projects')->delete();
-        DB::table('services')->delete();
-        DB::table('customers')->delete();
-        DB::table('users')->delete();
-
-        $this->call('UserTableSeeder');
-        $this->call('CustomerTableSeeder');
-        $this->call('ProjectTableSeeder');
-        $this->call('ServiceTableSeeder');
-        $this->call('ActivityTableSeeder');
-        $this->call('TimesliceTableSeeder');
+        $this->seedDimeCore();
     }
 }
