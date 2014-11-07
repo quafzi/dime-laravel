b7b8ab48a7efeeae76fd0f023a78e7dde61bdf8e
Thomas Birke <quafzi@netextreme.de>
2014-11-07 12:00:39 +0100

    Add Eloquence for camelCase field names

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
+        'Eloquence\EloquenceServiceProvider',
         'Dime\Api\ApiServiceProvider',
         'Dime\Core\CoreServiceProvider',
 	),
